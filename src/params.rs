eaa0de2964df4769b9ff3160ab66bec54793a9f1
Sean Bowe <ewillbefull@gmail.com>
2016-06-29 22:00:22 -0600

    Arithmetic in Fp

--- src/params.rs
+++ src/params.rs
@@ -8,6 +8,7 @@ impl PrimeFieldParams for FrParams {
         BigUint::from_str_radix("21888242871839275222246405745257275088548364400416034343698204186575808495617", 10).unwrap()
     }
     fn bits() -> usize { 254 }
+    fn name() -> &'static str { "Fr" }
 }
 
 pub struct FqParams;
@@ -17,4 +18,5 @@ impl PrimeFieldParams for FqParams {
         BigUint::from_str_radix("21888242871839275222246405745257275088696311157297823662689037894645226208583", 10).unwrap()
     }
     fn bits() -> usize { 254 }
+    fn name() -> &'static str { "Fq" }
 }
